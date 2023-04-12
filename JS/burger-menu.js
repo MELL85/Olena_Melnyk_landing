20ec752e57a613354c53dc9fc1a00d93b9664e58
Yevhen <melnyk_evgenij@icloud.com>
2023-04-12 14:18:00 +0300

    change btn in section btn and move testimonials to section answer

--- JS/burger-menu.js
+++ JS/burger-menu.js
@@ -1,14 +1,3 @@
-// (() => {
-// const burger = document.querySelector('#burgermenu-btn');
-// const menu = document.querySelector('#burgermenu');
-
-// burger.addEventListener('click', () => {
-//     menu.classList.toggle('menu__down');
-// })
-// })();
-
-
-
 (() => {
     const iconBurger = document.querySelector("#burgermenu-btn");
     const navigationsBody = document.querySelector("#burgermenu");
@@ -25,16 +14,4 @@
         menu.classList.toggle('menu__down');
     })
 
-
-    // const navLinks = document.querySelectorAll(".header__burgermenu-title");
-    // navLinks.forEach((navLink) => {
-    //   navLink.addEventListener("click", (_) => {
-    //     if (iconBurger.classList.contains("active")) {
-    //       document.body.classList.remove("lock");
-    //       iconBurger.classList.remove("active");
-    //       navigationsBody.classList.remove("active");
-    //     }
-    //   }
-    //   );
-    // });
 })();
