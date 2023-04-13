96a3e34c10ac4c8652c5f3dfa2b360c885abdac7
Yevhen <melnyk_evgenij@icloud.com>
2023-04-13 18:34:50 +0300

    added caroussel to testimonials with js del bootstrap min js

--- JS/slider-coverflow.js
+++ JS/slider-coverflow.js
@@ -1,5 +1,34 @@
 (() => {
 
-  
+    $(document).ready(function () {
+        $('.slider').slick({
+            arrows: true,
+            dots: true,
+            adaptiveHeight: true,
+            slidesToShow: 1,
+            speed: 500,
+            easing: 'ease-in-out',
+            // waitForAnimate: false,
+     
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 2,
+                        centerMode: true,
+                    }
+                }, 
+                {
+                    breakpoint: 992,
+                    settings: {
+                        centerMode: true,
+                        slidesToShow: 3,
+                    }
+                }
+            ],
+            mobileFirst: true,
+
+        });
+    });
 
 })();
