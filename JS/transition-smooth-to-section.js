1edba40909b357125a7c7bfd5ef9c4219ab4dc9a
Yevhen <melnyk_evgenij@icloud.com>
2023-04-26 17:29:07 +0300

    corrected js transition smooth to btn and correct tag a to btn

--- JS/transition-smooth-to-section.js
+++ JS/transition-smooth-to-section.js
@@ -1,6 +1,6 @@
 (() => {
     $(document).ready(function () {
-        $('#navbar').on('click', '.header__burgermenu-title', function (event) {
+        $('#navbar').on('click', '.my__fixed-transition-point', function (event) {
             event.preventDefault();
 
             const id = $(this).attr('href'),
@@ -11,13 +11,30 @@
     });
 
     $(document).ready(function () {
-        $('#navbar-desctop').on('click', '.header__burgermenu-title', function (event) {
+        $('#navbar-desctop').on('click', '.my__fixed-transition-point', function (event) {
             event.preventDefault();
 
             const id = $(this).attr('href'),
                 top = $(id).offset().top;
 
             $('body,html').animate({ scrollTop: top - 99 }, 500);
+        });
+    });
+
+    $(document).ready(function () {
+        $('#linkBtn').on('click', '.my__fixed-transition-point', function (event) {
+            event.preventDefault();
+
+            const id = $(this).attr('href'),
+                top = $(id).offset().top;
+
+            // $('body,html').animate({ scrollTop: top - 99 }, 500);
+
+            if (document.documentElement.clientWidth < 992) {
+                $('body,html').animate({ scrollTop: top - 74 }, 500);
+            } else {
+                $('body,html').animate({ scrollTop: top - 99 }, 500);
+            }
         });
     });
 })();
